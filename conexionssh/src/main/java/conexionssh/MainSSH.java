1d3f7df34812dcbbaba0949bcad7a6a1bb97658d
SusanaSanch <susanasanchez@auxiliamadrid.org>
2017-02-06 20:13:01 +0100

    conexionssh

--- conexionssh/src/main/java/conexionssh/MainSSH.java
+++ conexionssh/src/main/java/conexionssh/MainSSH.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
