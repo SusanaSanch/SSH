b65d0b855534a77862ff156c72e76e3fb8f4bed3
SusanaSanch <susanasanchez@auxiliamadrid.org>
2017-02-06 21:11:59 +0100

    Patolog visuales susana

--- conexionssh/src/main/java/patologiasvisualessusana/Patologia.java
+++ conexionssh/src/main/java/patologiasvisualessusana/Patologia.java
@@ -1,4 +1,4 @@
-package patologiasvisuales;
+package patologiasvisualessusana;
 
 public class Patologia {
 	
--- conexionssh/src/main/java/patologiasvisualessusana/Principal.java
+++ conexionssh/src/main/java/patologiasvisualessusana/Principal.java
@@ -1,4 +1,4 @@
-package patologiasvisuales;
+package patologiasvisualessusana;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
